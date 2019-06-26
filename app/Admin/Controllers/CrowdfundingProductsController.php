650d7d730b73db2d6020b153132ddec32983513a
chunhuili <2947680759@qq.com>
2019-06-26 16:30:19 +0800

    add 管理后台商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -14,7 +14,34 @@ use Encore\Admin\Show;
 
 class CrowdfundingProductsController extends Controller
 {
-    use HasResourceActions;
+    // 移除 HasResourceActions
+    public function getProductType()
+    {
+        return Product::TYPE_CROWDFUNDING;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', ' 状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 众筹相关字段
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+    }
 
     /**
      * Index interface.
@@ -22,12 +49,12 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('众筹商品列表')
-            ->body($this->grid());
-    }
+//    public function index(Content $content)
+//    {
+//        return $content
+//            ->header('众筹商品列表')
+//            ->body($this->grid());
+//    }
 
     /**
      * Show interface.
@@ -51,12 +78,12 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑众筹商品')
-            ->body($this->form()->edit($id));
-    }
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('编辑众筹商品')
+//            ->body($this->form()->edit($id));
+//    }
 
     /**
      * Create interface.
@@ -64,12 +91,12 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建众筹商品')
-            ->body($this->form());
-    }
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('创建众筹商品')
+//            ->body($this->form());
+//    }
 
     /**
      * Make a grid builder.
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -4,16 +4,39 @@ namespace App\Admin\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
-use App\Http\Controllers\Controller;
-use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
-    use HasResourceActions;
+//    use HasResourceActions;
+
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
+    }
 
     /**
      * Index interface.
@@ -21,13 +44,13 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('Index')
-            ->description('商品列表')
-            ->body($this->grid());
-    }
+//    public function index(Content $content)
+//    {
+//        return $content
+//            ->header('Index')
+//            ->description('商品列表')
+//            ->body($this->grid());
+//    }
 
     /**
      * Show interface.
@@ -51,12 +74,12 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑商品')
-            ->body($this->form()->edit($id));
-    }
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('编辑商品')
+//            ->body($this->form()->edit($id));
+//    }
 
     /**
      * Create interface.
@@ -64,13 +87,13 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('Create')
-            ->description('创建商品')
-            ->body($this->form());
-    }
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('Create')
+//            ->description('创建商品')
+//            ->body($this->form());
+//    }
 
     /**
      * Make a grid builder.
