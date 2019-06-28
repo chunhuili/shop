d2df80128d069a534cbfb72b49acba2ba72be8a3
chunhuili <2947680759@qq.com>
2019-06-28 11:07:24 +0800

    add 众筹结束逻辑

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -207,7 +207,7 @@ class OrdersController extends Controller
         return redirect()->back();
     }
 
-    public function handleRefund(Order $order,ApplyRefundRequest $request)
+    public function handleRefund(Order $order,ApplyRefundRequest $request,OrderService $orderService)
     {
         // 判断订单状态是否正确
         if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
@@ -222,7 +222,9 @@ class OrdersController extends Controller
                 'extra' => $extra,
             ]);
             // 调用退款逻辑
-            $this->_refundOrder($order);
+//            $this->_refundOrder($order);
+            // 改为调用封装的方法
+            $orderService->refundOrder($order);
         } else {
             // 将拒绝退款理由放到订单的 extra 字段中
             $extra = $order->extra ?: [];
