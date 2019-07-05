11f38d865b28c28022f3f837b66a8084ac75a34c
chunhuili <2947680759@qq.com>
2019-07-05 09:56:16 +0800

    add "同步商品数据至 Elasticsearch"

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SyncOneProductToES;
 use App\Models\Category;
 use App\Models\Product;
 use Encore\Admin\Controllers\HasResourceActions;
@@ -79,28 +80,29 @@ abstract class CommonProductsController extends Controller
         $form->image('image', '封面图片')->rules('required|image');
         $form->editor('description', '商品描述')->rules('required');
         $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-
         // 调用自定义方法
         $this->customForm($form);
-
         $form->hasMany('skus', '商品 SKU', function (Form\NestedForm $form) {
             $form->text('title', 'SKU 名称')->rules('required');
             $form->text('description', 'SKU 描述')->rules('required');
             $form->text('price', '单价')->rules('required|numeric|min:0.01');
             $form->text('stock', '剩余库存')->rules('required|integer|min:0');
         });
-
-//        $form->hasMany('properties', '商品属性', function (Form\NestedForm $form) {
-//            $form->text('name', '属性名')->rules('required');
-//            $form->text('value', '属性值')->rules('required');
-//        });
-
+        // 放在 SKU 下面
+        $form->hasMany('properties', '商品属性', function (Form\NestedForm $form) {
+            $form->text('name', '属性名')->rules('required');
+            $form->text('value', '属性值')->rules('required');
+        });
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
         });
-
+        $form->saved(function (Form $form) {
+            $product = $form->model();
+            $this->dispatch(new SyncOneProductToES($product));
+        });
         return $form;
     }
+
 
     // 定义一个抽象方法，各个类型的控制器将实现本方法来定义表单应该有哪些额外的字段
     abstract protected function customForm(Form $form);
