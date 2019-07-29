ed244656d636bf835ee3ca0dcfb4841f41662540
chunhuili <2947680759@qq.com>
2019-07-29 10:47:09 +0800

    add 代码封装

--- app/SearchBuilders/ProductSearchBuilder.php
+++ app/SearchBuilders/ProductSearchBuilder.php
@@ -5,26 +5,21 @@ use App\Models\Category;
 
 class ProductSearchBuilder
 {
-    //初始化查询
+    // 初始化查询
     protected $params = [
         'index' => 'products',
-        'type' => '_doc',
-        'body' => [
-            'query' =>[
+        'type'  => '_doc',
+        'body'  => [
+            'query' => [
                 'bool' => [
                     'filter' => [],
-                    'must' => [],
-                ]
-            ]
-        ]
+                    'must'   => [],
+                ],
+            ],
+        ],
     ];
 
-    /**
-     * 添加分页查询
-     * @param $size
-     * @param $page
-     * @return $this
-     */
+    // 添加分页查询
     public function paginate($size, $page)
     {
         $this->params['body']['from'] = ($page - 1) * $size;
@@ -33,51 +28,32 @@ class ProductSearchBuilder
         return $this;
     }
 
-    /**
-     * 筛选上架状态
-     * @return $this
-     */
+    // 筛选上架状态的商品
     public function onSale()
     {
-        $this->params['body']['query']['bool']['filter'][] = [
-            'term' => [
-                'on_sale' => true
-            ]
-        ];
+        $this->params['body']['query']['bool']['filter'][] = ['term' => ['on_sale' => true]];
 
         return $this;
     }
 
-    /**
-     * 按类目筛选商品
-     * @param Category $category
-     * @return $this
-     */
+    // 按类目筛选商品
     public function category(Category $category)
     {
         if ($category->is_directory) {
             $this->params['body']['query']['bool']['filter'][] = [
-                'prefix' => ['category_path' => $category->path.$category->id.'-']
+                'prefix' => ['category_path' => $category->path.$category->id.'-'],
             ];
         } else {
-            $this->params['body']['query']['bool']['filter'][] = [
-                'term' => [
-                    'category_id' => $category->id
-                ]
-            ];
+            $this->params['body']['query']['bool']['filter'][] = ['term' => ['category_id' => $category->id]];
         }
 
         return $this;
     }
 
-    /**
-     * 添加搜索词
-     * @param $keywords
-     * @return $this
-     */
+    // 添加搜索词
     public function keywords($keywords)
     {
-        //如果参数不是数组则转为数组
+        // 如果参数不是数组则转为数组
         $keywords = is_array($keywords) ? $keywords : [$keywords];
         foreach ($keywords as $keyword) {
             $this->params['body']['query']['bool']['must'][] = [
@@ -92,86 +68,68 @@ class ProductSearchBuilder
                         'skus_description',
                         'properties_value',
                     ],
-                ]
+                ],
             ];
         }
 
         return $this;
     }
 
-    /**
-     * 分面搜索的聚合
-     * @return $this
-     */
+    // 分面搜索的聚合
     public function aggregateProperties()
     {
         $this->params['body']['aggs'] = [
             'properties' => [
                 'nested' => [
-                    'path' => 'properties'
+                    'path' => 'properties',
                 ],
-                'agg' => [
+                'aggs'   => [
                     'properties' => [
                         'terms' => [
                             'field' => 'properties.name',
                         ],
-                        'aggs' => [
+                        'aggs'  => [
                             'value' => [
                                 'terms' => [
                                     'field' => 'properties.value',
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
         ];
 
         return $this;
     }
 
-    /**
-     * 按商品属性筛选条件
-     * @param $name
-     * @param $value
-     * @return $this
-     */
+    // 添加一个按商品属性筛选的条件
     public function propertyFilter($name, $value)
     {
         $this->params['body']['query']['bool']['filter'][] = [
             'nested' => [
-                'path' => 'properties',
+                'path'  => 'properties',
                 'query' => [
-                    'term' => ['properties.search_value' => $name.':'.$value]
-                ]
-            ]
+                    ['term' => ['properties.search_value' => $name.':'.$value]],
+                ],
+            ],
         ];
 
         return $this;
     }
 
-    /**
-     * 添加排序
-     * @param $field
-     * @param $direction
-     * @return $this
-     */
+    // 添加排序
     public function orderBy($field, $direction)
     {
         if (!isset($this->params['body']['sort'])) {
             $this->params['body']['sort'] = [];
         }
-
         $this->params['body']['sort'][] = [$field => $direction];
 
         return $this;
     }
 
-
-    /**
-     * 返回构造好的查询参数
-     * @return array
-     */
+    // 返回构造好的查询参数
     public function getParams()
     {
         return $this->params;
